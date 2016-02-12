8ef5c8db4bb941d02c68ed54971b47e53a218c7a
Nicola Wilborn <nicola.wilborn@gmail.com>
2016-02-11 16:27:40 -0800

    The original commit for assignment1

---
+++ main.cpp
@@ -0,0 +1,40 @@
+//
+//  main.cpp
+//  Assignment1
+//
+//  Created by Nicola Wilborn on 1/11/16.
+//  Copyright © 2016 Nicola Wilborn. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!\n\n";
+   cout << "My family name is Wilborn" << endl;
+   cout << "My student ID is 20258926" << endl << endl;
+   cout << "Syllabus detail #1: The instructor for this course is Michael Loceff.\n"
+      << "Syllabus detail #2: Our lab assignments are worth 75% of our final grade.\n"
+      << "Specific lab detail #1: It is better to submit an assignment late, than submit a non-working program. \n"
+      << "Specific lab detail #2: All tabs should be removed, and replaced with three spaces.\n"
+      << "Specific lab detail #3: Assignments must be our own work; cheating is unacceptable."<< endl << endl;
+   return 0;
+}
+
+/* ---------------------- RUN -------------------------------------
+
+Hello World!
+
+My family name is Wilborn
+My student ID is 20258926
+
+Syllabus detail #1: The instructor for this course is Michael Loceff.
+Syllabus detail #2: Our lab assignments are worth 75% of our final grade.
+Specific lab detail #1: It is better to submit an assignment late, than submit a non-working program. 
+Specific lab detail #2: All tabs should be removed, and replaced with three spaces.
+Specific lab detail #3: Assignments must be our own work; cheating is unacceptable.
+
+Program ended with exit code: 0
+
+ ------------------------------------------------------------------ */
